a2f6243937b4c39ae7f3a137fbb9b50c0f5af36f
Patrick Avery <psavery@buffalo.edu>
2015-11-11 14:42:27 -0500

    Made the timer more user-friendly and enforced 6 decimal places for wall-time through the printf() function.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,42 @@
 /* Written by Patrick S. Avery -- 2015
  *
- * This is just a basic timer compiled with CMake.
- * It prints wall-time to about 4 decimal places and
- * cpu-time to about 2 decimal places
+ * This is just a basic timer whose makefile is generated with CMake.
+ * It prints wall-time to 6 decimal places and
+ * cpu-time to 2 decimal places
  */
 
 #include <chrono>
+#include <cstdio>
 #include <iostream>
+
+void timedFunction();
+void timeIt(void (*timedFunction)());
+
+int main(int argc, char* argv[])
+{
+  timeIt(timedFunction);
+  // Add timeIt() with different functions to be called here
+}
+
+// Add functions to be called here
 
 void timedFunction()
 {
-  // Insert timed function call here
+  for (size_t i = 0; i < 100000; i++) std::cout << i << "\n";
+  // Or insert a single timed function call here
 }
 
-int main(int argc, char* argv[])
+void timeIt(void (*timedFunction)())
 {
   auto start_cpu = clock();
   auto start_wall = std::chrono::high_resolution_clock::now();
 
-  timedFunction();
+  (*timedFunction)();
 
   auto cpuTime = (clock() - start_cpu) / (double) CLOCKS_PER_SEC;
   auto wallTime = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - start_wall).count() * 0.000000001;
 
-  std::cout << "wallTime is " << wallTime << "\n";
-  std::cout << "cpuTime is " << cpuTime << "\n";
+  printf("wallTime is %.6f\n", wallTime);
+  printf("cpuTime is %.2f\n", cpuTime);;
 }
+
